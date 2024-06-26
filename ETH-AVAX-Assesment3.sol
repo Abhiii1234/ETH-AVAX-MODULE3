02fca0f11af8f1238d6b2d84d15a0d5f6729cfea
Abhigyan Pushkar <149153539+Abhiii1234@users.noreply.github.com>
2024-06-27 01:30:39 +0530

    Update ETH-AVAX-Assesment3.sol

--- ETH-AVAX-Assesment3.sol
+++ ETH-AVAX-Assesment3.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) Ownable(msg.sender) {
-        // Initial owner is set by the Ownable constructor
+contract MyToken is ERC20 {
+    address public owner;
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
+        
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
     }
 
     function mint(address account, uint256 amount) public onlyOwner {
